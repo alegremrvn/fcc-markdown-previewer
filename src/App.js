8ac4b200346a2b1cd150de9e7e6ce9d9bb0f6492
Marvin Alegre <alegremrvn@protonmail.com>
2022-07-10 08:21:37 +0800

    good enough

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { marked } from 'marked';
+import React from 'react';
 import './App.css';
+
+class MarkdownPreviewer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '# Welcome!\n\n## This is a  subheading...\n### And here\'s some other cool stuff:\n\nHers some code, `<div></div>`, between 2 backticks.\n\n```//// this is a multi-line code:\n\nfunction anotherExample(firstLine, lastLine) {\n  if (firstLine == \'```\' && lastLine == \'```\') {\n    return multiLineCode:\n  }\n}\n```\n**bold** text\n1. numbered list\n1. adf\n1. asdf\n> block quotes!\n\n![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)\n**bold**\nhow dare you forget to add a [link](https://www.freecodecamp.org)!!!'
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      input: e.target.value
+    });
+  }
+  shouldComponentUpdate() {
+    return true;
+  }
+
+  render() {
+    document.getElementById('preview').innerHTML = marked.parse(this.state.input);
+    return (
+      <div>
+        <textarea id="editor" value={this.state.input} onChange={this.handleChange}></textarea>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MarkdownPreviewer />
     </div>
   );
 }
